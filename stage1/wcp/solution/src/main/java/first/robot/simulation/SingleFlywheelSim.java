1835314a1b0bedceb7d09cca77b73ccf8aa92a87
Spaceman 11 <135277197+Spaceman113138@users.noreply.github.com>
2026-06-15 23:38:18 -0500

    add simulation files

---
+++ stage1/wcp/solution/src/main/java/first/robot/simulation/SingleFlywheelSim.java
@@ -0,0 +1,60 @@
+package first.robot.simulation;
+
+import org.wpilib.math.system.DCMotor;
+import org.wpilib.math.system.Models;
+import org.wpilib.networktables.DoublePublisher;
+import org.wpilib.networktables.NetworkTable;
+import org.wpilib.networktables.NetworkTableInstance;
+import org.wpilib.simulation.BatterySim;
+import org.wpilib.simulation.FlywheelSim;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class SingleFlywheelSim {
+
+    private final TalonFX talonMotor;
+    private double motorPosition = 0.0;
+
+    private final double gearRatio = 1.0;
+    private final FlywheelSim flywheelSim =
+        new FlywheelSim(
+            Models.flywheelFromPhysicalConstants(
+                DCMotor.getKrakenX60(1), 
+                0.001, 
+                gearRatio),
+            DCMotor.getKrakenX60(1));
+
+    private final double kBusVoltage = 12.0;
+    
+    private final DoublePublisher motorVoltagePub;
+    private final DoublePublisher motorVelocityPub;
+    private final DoublePublisher motorCurrentPub;
+    private final DoublePublisher motorPositionPub;
+
+
+    public SingleFlywheelSim(TalonFX talonMotor, String name) {
+        this.talonMotor = talonMotor;
+
+        var table = NetworkTableInstance.getDefault().getTable(name);
+        motorVoltagePub = table.getDoubleTopic("MotorVoltage").publish();
+        motorVelocityPub = table.getDoubleTopic("MotorVelocity").publish();
+        motorCurrentPub = table.getDoubleTopic("MotorCurrent").publish();
+        motorPositionPub = table.getDoubleTopic("MotorPosition").publish();
+    }
+
+    public void periodic() {
+        double motorVoltage = talonMotor.getThrottle() * kBusVoltage;
+        
+        flywheelSim.setInputVoltage(motorVoltage);
+        flywheelSim.update(0.02);
+
+        double motorVelo = flywheelSim.getAngularVelocity();
+        motorPosition += motorVelo * 0.02;
+
+        motorVoltagePub.set(motorVoltage);
+        motorVelocityPub.set(motorVelo);
+        motorCurrentPub.set(flywheelSim.getCurrentDraw());
+        motorPositionPub.set(motorPosition);
+
+    }
+}
