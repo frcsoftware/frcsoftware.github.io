7a988954d85c06e15b53f6ccb982be4c7949c455
samfreund <samf.236@proton.me>
2026-06-20 15:12:11 -0700

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,7 +16,7 @@ export default defineConfig({
             remarkFigure,
             remarkGlossary,
             remarkImageAttributes,
-	    remarkMdxGlobalImports
+            remarkMdxGlobalImports,
         ],
         rehypePlugins: [],
     },
