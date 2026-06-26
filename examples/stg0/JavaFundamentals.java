f5a66c0c6a5d379682b50a4980677c82730c058c
samfreund <samf.236@proton.me>
2026-06-25 20:03:35 -0700

    fix java-funds

---
+++ examples/stg0/JavaFundamentals.java
@@ -0,0 +1,31 @@
+// #region variables
+int CLIMBER_ID = 51;
+double UP_POSITION = -33.5;
+double DOWN_POSITION = 0;
+// #endregion variables
+
+void main() {
+    // #region printLiteral
+    System.out.print("hello!");
+    // #endregion printLiteral
+
+    // #region printVariable
+    int number = 4;
+    System.out.print(number); // prints out the value 4
+    // #endregion printVariable
+
+    // #region singleLineComment
+    // This prints Hello World
+    System.out.print("Hello World");
+    // #endregion singleLineComment
+
+    // #region inlineComment
+    System.out.print("Hello World"); // This prints Hello World
+    // #endregion inlineComment
+
+    // #region multiLineComment
+    /* This prints Hello World
+    This is another line */
+    System.out.print("Hello World");
+    // #endregion multiLineComment
+}
