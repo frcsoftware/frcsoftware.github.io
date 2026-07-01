1d7b01a9437b69d7afb2c726bbdef39f2e52907e
samfreund <samf.236@proton.me>
2026-07-01 00:17:39 -0700

    init

--- src/data/glossary.ts
+++ src/data/glossary.ts
@@ -159,6 +159,32 @@ export const glossaryTerms: GlossaryTerm[] = [
         definition:
             'Device that helps the robot communicate with the driver station',
     },
+    {
+        term: 'Main Breaker',
+        definition: 'Switch for the robot',
+    },
+    {
+        term: 'Limit Switch',
+        definition:
+            'Type of sensor that acts as a soft limit when physically or magnetically hit',
+    },
+    {
+        term: 'Throughbore Encoder',
+        definition:
+            'An encoder that allows shafts to pass through its center to record position',
+    },
+    {
+        term: 'Magnetic Encoder',
+        definition:
+            'An encoder that uses magnetic fields to measure position or motion',
+    },
+
+    // Software
+    {
+        term: 'Repository',
+        definition:
+            'A storage location for software packages, often used in version control systems like Git. Repositories are just folders that contain files and subfolders, and they can be hosted on platforms like GitHub to facilitate collaboration and version tracking',
+    },
 
     // Manufacturing & Materials
     {
