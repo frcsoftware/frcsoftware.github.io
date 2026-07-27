c6acf5813ad3e1268e35d0287ba36a0a7b93845a
httphypixelnet <intenstudios369@gmail.com>
2026-07-27 12:50:08 -0700

    Add pre-push and post-checkout hooks
    Modified ESLint and lint-staged configs to better support modular and conditional linting
    Should save time in creating commits and further reduce risk of outdated node_modules

---
+++ lint-staged.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+
+/** @type {import('lint-staged').Configuration} */
+export default {
+    '**/*': (files) => `prettier --write ${files.join(' ')}`,
+    '**/*.{astro,ts,mjs,js}': (files) => `eslint --fix ${files.join(' ')}`,
+    '**/*.{md,mdx}': (files) => [
+        `pnpm remark  ${files.join(' ')} --ext mdx --frail --no-stdout --quiet`,
+    ],
+    'examples/**/*.{java,gradle}': () => [
+        `./examples/gradlew -p examples spotlessApply`,
+    ],
+    'src/data/glossary.ts': () => [
+        'pnpm generate:glossary',
+        'git add src/content/docs/resources/glossary.mdx',
+    ],
+    // Yes, I know this should be a FunctionTask but those are kinda bad until https://github.com/lint-staged/lint-staged/issues/1826 is resolved
+    'package.json': () => 'pnpm tsx scripts/syncLockfile.lint.ts',
+};
