fc7091c67b8dafbe9b961972d23d636bcb82c34a
httphypixelnet <intenstudios369@gmail.com>
2026-08-05 11:23:18 -0700

    use sharp to validate image types

--- scripts/images.lint.js
+++ scripts/images.lint.js
@@ -1,7 +1,24 @@
 import { styleText } from 'node:util';
+import sharp from 'sharp';
 
 const [, , ...images] = process.argv;
-process.stderr.write(
-    `${styleText('red', '✖')} All images should be in WebP format. See contribution guidelines for more info. Offending files: \n - ${images.map((img) => img.replace(process.cwd() + '/', '')).join('\n - ')}\n`,
-);
-process.exit(1);
+
+/** @type {string[]} */
+const offending = [];
+
+for (const image of images) {
+    if (!image.endsWith('.webp')) {
+        offending.push(image);
+        continue;
+    }
+    const meta = await sharp(image).metadata();
+    if (meta.format !== 'webp') {
+        offending.push(image);
+    }
+}
+if (offending.length >= 1) {
+    process.stderr.write(
+        `${styleText('red', '✖')} All images should be in WebP format. See contribution guidelines for more info. Offending files: \n - ${offending.map((img) => img.replace(process.cwd() + '/', '')).join('\n - ')}\n`,
+    );
+    process.exit(1);
+}
