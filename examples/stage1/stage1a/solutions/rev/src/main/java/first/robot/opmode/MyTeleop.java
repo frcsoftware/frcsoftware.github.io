f31331a7d93fbb8615a03007bdd2b7b2157e0705
Spaceman 11 <135277197+Spaceman113138@users.noreply.github.com>
2026-08-11 18:20:19 -0500

    apply suggestions

--- examples/stage1/stage1a/solutions/rev/src/main/java/first/robot/opmode/MyTeleop.java
+++ examples/stage1/stage1a/solutions/rev/src/main/java/first/robot/opmode/MyTeleop.java
@@ -18,12 +18,13 @@ public class MyTeleop extends PeriodicOpMode {
   private final NiDsXboxController xboxController = new NiDsXboxController(0);
 
   // [/Controller]
-
+  /** The Robot instance is passed into the opmode via the constructor. */
   public MyTeleop(Robot robot) {
     this.robot = robot;
   }
 
   // [DriveSimPeriodic]
+  /* Called periodically (set time interval) while the robot is enabled. */
   @Override
   public void periodic() {
     robot.drivetrain.arcadeDrive(-xboxController.getLeftY(), xboxController.getRightX());
