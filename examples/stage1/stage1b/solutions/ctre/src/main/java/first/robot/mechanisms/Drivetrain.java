b7515fd38b444e381d663eae07e349c1d35c7a51
Daniel1464 <danielchen1464@gmail.com>
2026-09-03 08:44:00 -0400

    Switch to different secondary auto mode

--- examples/stage1/stage1b/solutions/ctre/src/main/java/first/robot/mechanisms/Drivetrain.java
+++ examples/stage1/stage1b/solutions/ctre/src/main/java/first/robot/mechanisms/Drivetrain.java
@@ -18,7 +18,6 @@ import org.wpilib.command3.Mechanism;
 import org.wpilib.drive.DifferentialDrive;
 import org.wpilib.hardware.imu.OnboardIMU;
 import org.wpilib.hardware.imu.OnboardIMU.MountOrientation;
-import org.wpilib.math.util.MathUtil;
 
 public class Drivetrain implements Mechanism {
   private static final int leftLeaderID = 0, rightLeaderID = 2;
@@ -67,19 +66,6 @@ public class Drivetrain implements Mechanism {
           }
         })
         .named("Drive");
-  }
-
-  public Command rotateInPlace(double angleDegrees, DoubleSupplier rotationThrottle) {
-    return run(coroutine -> {
-          double targetAngle =
-              MathUtil.inputModulus(imu.getRotation2d().getDegrees() + angleDegrees, -180, 180);
-          while (imu.getRotation2d().getDegrees() < targetAngle) {
-            differentialDrive.arcadeDrive(0.0, rotationThrottle.getAsDouble());
-            coroutine.yield();
-          }
-          differentialDrive.arcadeDrive(0.0, 0.0);
-        })
-        .named("RotateInPlace");
   }
 
   public void periodic() {
